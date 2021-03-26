b757d181506d3d32025db2f3ddca519c98540d81
chat43mcn <81398510+chat43mcn@users.noreply.github.com>
2021-03-25 20:07:14 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyAdLmJJe5p2ufEMQwwTLGg4zUEo235naj4",
+    authDomain: "chatkus1.firebaseapp.com",
+    projectId: "chatkus1",
+    storageBucket: "chatkus1.appspot.com",
+    messagingSenderId: "555426460233",
+    appId: "1:555426460233:web:f295482ed3204e5e174598",
+    measurementId: "G-55EEES53YM"
+    });
